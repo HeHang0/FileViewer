c8e2613c096a607d3999dec37b95f3e9d223bbc5
HeHang <hehang724590957@qq.com>
2019-04-21 23:09:27 +0800

    【添加】项目

---
+++ ViewModel/Eventer.cs
@@ -0,0 +1,58 @@
+﻿using FileViewer.Monitor;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace FileViewer.ViewModel
+{
+    public class Eventer
+    {
+        public ICommand OnLoaded => new DelegateCommand<System.Windows.Window>((win) => {
+            Loaded?.Invoke(null, null);
+            InitHotKey(win);
+        });
+
+        public delegate void ReceiveFileEventHandler(object sender, string msg);
+        public event ReceiveFileEventHandler ReceiveFile;
+        void OnReceiveFile(string msg)
+        {
+            ReceiveFile?.Invoke(this, msg);
+        }
+
+        public event EventHandler Loaded;
+
+
+        private void InitHotKey(System.Windows.Window win)
+        {
+            HwndSource hWndSource;
+            WindowInteropHelper wih = new WindowInteropHelper(win);
+            hWndSource = HwndSource.FromHwnd(wih.Handle);
+            //添加处理程序 
+            hWndSource.AddHook(MainWindowProc);
+            HotKey.RegisterHotKey(wih.Handle, HotKey.GlobalAddAtom("Space"), HotKey.KeyModifiers.None, (int)System.Windows.Forms.Keys.Space);
+        }
+
+        private IntPtr MainWindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case HotKey.WM_HOTKEY:
+                    {
+                        handled = true;
+                        var (ok, filePath) = ExplorerFile.GetCurrentFilePath();
+                        if (ok)
+                        {
+                            OnReceiveFile(filePath);
+                        }
+                        break;
+                    }
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ ViewModel/FileAccess.cs
@@ -0,0 +1,37 @@
+﻿using FileViewer.FileHelper;
+using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace FileViewer.ViewModel
+{
+    public class FileAccess : INotifyPropertyChanged
+    {
+        public delegate void SizeChangeEventHandler(double height, double width);
+        public event SizeChangeEventHandler SizeChange;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; private set; }
+
+        public FileExtension FType { get; private set; }
+
+        public BitmapSource ThumbnailImage { get; private set; }
+
+        public void InitFile(object sender, string filePath)
+        {
+            Title = Path.GetFileName(filePath);
+            FType = FileType.GetFileType(filePath);
+            ShellObject shellFile = ShellObject.FromParsingName(filePath);
+            ThumbnailImage = shellFile.Thumbnail.ExtraLargeBitmapSource;
+            SizeChange?.Invoke(ThumbnailImage.Height, ThumbnailImage.Width);
+        }
+    }
+}
